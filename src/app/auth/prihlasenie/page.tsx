8416678b4643354eb759509e313cfde335cca9a7
Alea2247 <alexandraachbergerova2007@gmail.com>
2024-11-04 12:45:02 +0100

    auth

---
+++ src/app/auth/prihlasenie/page.tsx
@@ -0,0 +1,11 @@
+
+// src/app/prihlasenie/page.tsx
+
+
+import SignInView from "../../../sections/SignInView";
+
+export const metadata = { title: "Prihlásenie užívateľa | Kutyl s.r.o." };
+
+export default function Prihlasenie() {
+  return <SignInView />;
+}
---
+++ src/app/auth/registracia/page.tsx
@@ -0,0 +1,11 @@
+
+// src/app/registracia/page.tsx
+
+
+import SignUpView from "../../../sections/SignUpView";
+
+export const metadata = { title: "Registrácia užívateľa | Kutyl s.r.o." };
+
+export default function Registracia() {
+  return <SignUpView />;
+}
